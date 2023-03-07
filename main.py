f07483b609f0bcda85e735b031d42b7893804b7b
AmirGolomb <gh.amir.251@gmail.com>
2023-03-08 00:26:55 +0200

    changed ex4 to store file information instead of file name

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import base64
 import datetime
-
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 from PIL import Image
 import pickle
-import codecs, json
 
 
 def decode32(st):
@@ -32,21 +30,6 @@ def show110():
     plt.show()
 
 
-def ex3t():
-    i = 0
-    for i in range(150):
-        to_open = 'C:/Users/dan-client/Downloads/Bar_python_exercise/ex3/' + str(i) + '/' + str(i) + '.pkl'
-        with open(to_open, 'rb') as handle:
-            b = pickle.load(handle)
-
-        to_print = ""
-        for key in b:
-            to_print += decode32(key) + " "
-            to_print += str(list(b[key].keys())) + " "
-            to_print += str(list((b[key].values()))[0]) + " "
-        print(to_print)
-
-
 def all_dates_in6():
     # return: a Dict[lil_num] -> List(Big num, folder) for each line in ex3/6/6.pkl
     to_open = 'C:/Users/dan-client/Downloads/Bar_python_exercise/ex3/6/6.pkl'
@@ -60,6 +43,7 @@ def all_dates_in6():
             all_dates_in_6[lil_num_key] = [b[folder_key][lil_num_key], decode32(folder_key)]
     return all_dates_in_6
 
+
 def all_ex1_unix_time():
     # return: List(big_num, lil_num) for each file in each folder in ex1
     path = 'C:/Users/dan-client/Downloads/Bar_python_exercise/ex1'
@@ -70,12 +54,10 @@ def all_ex1_unix_time():
             big_num, lil_num = removed_npz.split('_')
             #date = datetime.datetime.fromtimestamp(int(before_))
             all_ex1_unixtime.append([int(big_num), int(lil_num)])
-
     return all_ex1_unixtime
 
 
 def filename_daytime():
-    # return: dict[filepath] = daytime
     all_dates_in_6 = all_dates_in6()
     all_ex1_unixtime = all_ex1_unix_time()
     filename_daytime = {}
@@ -83,11 +65,11 @@ def filename_daytime():
         tot_unix_time = tzemed[0] + all_dates_in_6[tzemed[1]][0]
         path_to_file = 'C:/Users/dan-client/Downloads/Bar_python_exercise/ex1/' + all_dates_in_6[tzemed[1]][1] + '/'
         filename = str(tzemed[0]) + "_" + str(tzemed[1]) + ".npz"
-
         filename_daytime[filename] = [path_to_file, str(datetime.datetime.fromtimestamp(tot_unix_time))[:-9], all_dates_in_6[tzemed[1]][1]]
     return filename_daytime
 
-def ex3combined():
+
+def ex3rename():
     filename_daytime_ = filename_daytime()
     path = 'C:/Users/dan-client/Downloads/Bar_python_exercise/ex1'
     for root, dirs, files in os.walk(path):
@@ -98,24 +80,27 @@ def ex3combined():
 
 
 def ex4():
-    all_dates_all_fields = []
-    #np_all_dates_all_fields = np.array()
+    all_dates_all_fields = np.full([14, 76, 4016], "will change")
     path = 'C:/Users/dan-client/Downloads/Bar_python_exercise/ex1'
+    folder_index = 0
     for root, dirs, files in os.walk(path):
-        all_dates_in_field = []
-        for _file in files:
-            all_dates_in_field.append(_file)
-        all_dates_all_fields.append(all_dates_in_field)
-    all_dates_all_fields.pop(0)
-    all_dates_all_fields.pop(0)
+        for dir in dirs:
+            file_index = 0
+            for root1, dirs1, files1 in os.walk(path + '/' + dir):
+                for file in files1:
+                    file_in_list = np.load(path + '/' + dir + '/' + file, allow_pickle=True)["arr_0"]
+                    padding = np.append(file_in_list, [np.nan] * (4016 - file_in_list.shape[0]))
+                    for num_index in range(4016):
+                        all_dates_all_fields[folder_index, file_index, num_index] = padding[num_index]
+                    file_index += 1
+            folder_index += 1
+
     answer = np.array(all_dates_all_fields)
+    print(answer)
     np.savez('C:/Users/dan-client/Downloads/Bar_python_exercise/ex4/answer.npz', answer)
-    # print(answer)
 
 
 if __name__ == '__main__':
     ex4()
-
-
 
 
