94ae141b754de7797a07236523abcf5d14742b9a
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2023-03-08 16:16:48 +0200

    commit

---
+++ ‏‏assignment3/constants.py
@@ -0,0 +1,10 @@
+DOT_NPZ_SUFFIX = ".npz"
+DOT_PKL_SUFFIX = ".pkl"
+SIZE_OF_PREFIX_DOT_NPZ = len(DOT_NPZ_SUFFIX)
+MY_PROJECT_PATH = "C:\\Users\\AssafHillel\\Desktop\\"
+PATH_OF_EX1_DIRECTORY = MY_PROJECT_PATH + "python_assignment3\\ex1"
+
+PATH_OF_EX3_DIRECTORY = "C:\\Users\\AssafHillel\\Desktop\\python_assignment3\\ex3"
+SEPARATOR_BY_UNDERSCORE = '_'
+
+PICKLE_FULL_PATH = PATH_OF_EX3_DIRECTORY + "\\6\\6.pkl"
